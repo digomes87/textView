df6656673aa79fff6caf66ea5b0b1a90738af5b7
Nylon <diego.gomes87@gmail.com>
2015-11-03 15:44:24 -0200

    Initial Commit

---
+++ textViewTests/textViewTests.swift
@@ -0,0 +1,36 @@
+//
+//  textViewTests.swift
+//  textViewTests
+//
+//  Created by Diego Gomes on 03/11/2015.
+//  Copyright (c) 2015 Nylon. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class textViewTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
